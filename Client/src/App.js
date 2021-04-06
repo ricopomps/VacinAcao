00e5c3321f7f9149439297164d0b929900c61996
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-05 22:10:53 -0300

    Basic ui implementation

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
     <div>
+      <NavBar />
       <h1>VacinAcao</h1>
     </div>
   );
---
+++ Client/src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand>
+        <b>VacinAcao</b>
+      </Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default NavBar;
