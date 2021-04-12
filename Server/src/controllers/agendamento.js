7406a3a91c9ce279a77467298caad6422d68f767
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-12 17:45:22 -0300

    Implemented backend logic

--- Server/src/controllers/agendamento.js
+++ Server/src/controllers/agendamento.js
@@ -1,5 +1,6 @@
 import Agendamento from "../models/agendamento.js";
 import Dia from "../models/dia.js";
+import moment from "moment";
 import mongoose from "mongoose";
 
 export const getAgendamentos = async (req, res) => {
@@ -16,6 +17,57 @@ export const getAgendamentos = async (req, res) => {
 export const createAgendamentos = async (req, res) => {
   const agendamento = req.body;
   const newAgendamento = new Agendamento(agendamento);
+  const dia = await Dia.findOne({ date: newAgendamento.date });
+  if (dia.schedules ? dia.schedules.length > 19 : false) {
+    res.status(400).json({ message: "Não há mais vagas para esse dia" });
+  }
+  console.log(dia);
+  console.log(dia.date);
+  console.log(dia.schedules);
+  if (dia.schedules) {
+    console.log("AChou do dia");
+    if (
+      dia.schedules.find(
+        (schedule) => schedule.schedule === newAgendamento.schedule
+      )
+    ) {
+      console.log("AChou o schedule");
+
+      const schedules = dia.schedules.filter(
+        (schedule) => schedule.schedule === newAgendamento.schedule
+      );
+      if (schedules.length > 1) {
+        console.log("mais de 1 schedule");
+
+        const age = moment().diff(
+          moment(newAgendamento.age, "DD/MM/yyyy"),
+          "years"
+        );
+
+        const idosos = schedules.filter((schedule) => age > 60);
+        if (idosos.length > 1) {
+          console.log("1");
+
+          res.status(400).json({ message: "Não há mais vagas para esse dia" });
+          return;
+        }
+        if (age > 60) {
+          const schedulesOrdenado = dia.schedules.sort((a, b) => (a, b) => {
+            moment(moment(a.age, "DD/MM/yyyy")).diff(
+              moment(b.age, "DD/MM/yyyy")
+            );
+          });
+          await Agendamento.findByIdAndRemove(schedulesOrdenado[0].pacientId);
+        } else {
+          console.log("2");
+          res
+            .status(400)
+            .json({ message: "Vaga exclusiva para pessoas acimas de 60 anos" });
+          return;
+        }
+      }
+    }
+  }
   const newDay = new Dia({
     date: newAgendamento.date,
     schedules: {
@@ -34,11 +86,11 @@ export const createAgendamentos = async (req, res) => {
         if (error) {
           console.log(error);
         } else {
-          console.log(data);
+          // console.log(data);
         }
       }
     );
-    await res.status(201).json(agendamento);
+    res.status(201).json(agendamento);
   } catch (error) {
     console.log(error);
     res.status(404).json({ message: error.message });
