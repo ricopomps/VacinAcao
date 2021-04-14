eaa9dfb10cebc004ed09f3d51f5a929bc42329f6
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-14 17:16:52 -0300

    Visual changes

--- Client/src/Pages/Agendamento/Agendamento.js
+++ Client/src/Pages/Agendamento/Agendamento.js
@@ -3,13 +3,30 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import AgendamentoForm from "../../components/AgendamentoForm/AgendamentoForm";
 import Legenda from "../../components/Legenda/Legenda";
+import { IMAGE } from "../../components/NavBar/styles";
+import logo from "./../../image/logo.png";
 import Calendar from "../../components/Calendar/Calendar";
 import { TABLE_SCROLL } from "../../components/Calendar/styles";
 import Col from "react-bootstrap/Col";
 const Agendamento = () => {
   return (
     <Container>
-      <h1>VacinAcao</h1>
+      <Container style={{ display: "flex" }}>
+        <div style={{ margin: "auto" }}>
+          <Row>
+            <Col>
+              <IMAGE src={logo} />
+            </Col>
+            <Col>
+              <h1>VacinAcao</h1>
+            </Col>
+            <Col>
+              <IMAGE src={logo} />
+            </Col>
+          </Row>
+        </div>
+      </Container>
+
       <Row>
         <TABLE_SCROLL xs={8}>
           <Calendar />
