06d64f9c4a3db3338cc18f23da47801225fab209
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-15 11:50:33 -0300

    Implemented Ellipsis in bigger pages count

--- Client/src/components/Pagination/PaginationListagem.js
+++ Client/src/components/Pagination/PaginationListagem.js
@@ -9,19 +9,18 @@ const PaginationListagem = ({
 }) => {
   const pageNumbers = [];
 
-  for (
-    let i = 1;
-    i <= Math.ceil(totalAtendimentos / atendimentosPerPage);
-    i++
-  ) {
-    pageNumbers.push(i);
+  for (let i = currentPage - 2; i <= currentPage + 2; i++) {
+    i <= Math.ceil(totalAtendimentos / atendimentosPerPage) &&
+      i >= 1 &&
+      pageNumbers.push(i);
   }
   return (
     <Pagination>
-      <Pagination.First onClick={() => paginate(1)} />
+      <Pagination.First onClick={() => currentPage !== 1 && paginate(1)} />
       <Pagination.Prev
         onClick={() => currentPage > 1 && paginate(currentPage - 1)}
       />
+      {pageNumbers[0] > 1 && <Pagination.Ellipsis />}
       {pageNumbers.map((number) => (
         <Pagination.Item
           active={number === currentPage}
@@ -31,10 +30,18 @@ const PaginationListagem = ({
           {number}
         </Pagination.Item>
       ))}
+      {Math.ceil(totalAtendimentos / atendimentosPerPage) >
+        pageNumbers[pageNumbers.length - 1] && <Pagination.Ellipsis />}
       <Pagination.Next
         onClick={() =>
           currentPage < pageNumbers[pageNumbers.length - 1] &&
           paginate(currentPage + 1)
+        }
+      />
+      <Pagination.Last
+        onClick={() =>
+          currentPage !== pageNumbers[pageNumbers.length - 1] &&
+          paginate(pageNumbers[pageNumbers.length - 1])
         }
       />
     </Pagination>
