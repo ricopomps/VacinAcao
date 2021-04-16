b79fb0e09c2739b3b9fbf00b6d459d556fe86e35
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-15 21:19:25 -0300

    Css changes in pagination

--- Client/src/components/Pagination/PaginationListagem.js
+++ Client/src/components/Pagination/PaginationListagem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Pagination from "react-bootstrap/Pagination";
+import Container from "react-bootstrap/Container";
 const PaginationListagem = ({
   atendimentosPerPage,
   totalAtendimentos,
@@ -15,44 +16,46 @@ const PaginationListagem = ({
       pageNumbers.push(i);
   }
   return (
-    <Pagination>
-      <Pagination.First
-        disabled={currentPage === 1}
-        onClick={() => currentPage !== 1 && paginate(1)}
-      />
-      <Pagination.Prev
-        disabled={currentPage === 1}
-        onClick={() => currentPage > 1 && paginate(currentPage - 1)}
-      />
-      {pageNumbers[0] > 1 && <Pagination.Ellipsis disabled="true" />}
-      {pageNumbers.map((number) => (
-        <Pagination.Item
-          active={number === currentPage}
-          key={number}
-          onClick={() => paginate(number)}
-        >
-          {number}
-        </Pagination.Item>
-      ))}
-      {Math.ceil(totalAtendimentos / atendimentosPerPage) >
-        pageNumbers[pageNumbers.length - 1] && (
-        <Pagination.Ellipsis disabled="true" />
-      )}
-      <Pagination.Next
-        disabled={currentPage >= pageNumbers[pageNumbers.length - 1]}
-        onClick={() =>
-          currentPage < pageNumbers[pageNumbers.length - 1] &&
-          paginate(currentPage + 1)
-        }
-      />
-      <Pagination.Last
-        disabled={currentPage === pageNumbers[pageNumbers.length - 1]}
-        onClick={() =>
-          currentPage !== pageNumbers[pageNumbers.length - 1] &&
-          paginate(Math.ceil(totalAtendimentos / atendimentosPerPage))
-        }
-      />
-    </Pagination>
+    <Container style={{ display: "flex" }}>
+      <Pagination style={{ margin: "auto" }}>
+        <Pagination.First
+          disabled={currentPage === 1}
+          onClick={() => currentPage !== 1 && paginate(1)}
+        />
+        <Pagination.Prev
+          disabled={currentPage === 1}
+          onClick={() => currentPage > 1 && paginate(currentPage - 1)}
+        />
+        {pageNumbers[0] > 1 && <Pagination.Ellipsis disabled="true" />}
+        {pageNumbers.map((number) => (
+          <Pagination.Item
+            active={number === currentPage}
+            key={number}
+            onClick={() => paginate(number)}
+          >
+            {number}
+          </Pagination.Item>
+        ))}
+        {Math.ceil(totalAtendimentos / atendimentosPerPage) >
+          pageNumbers[pageNumbers.length - 1] && (
+          <Pagination.Ellipsis disabled="true" />
+        )}
+        <Pagination.Next
+          disabled={currentPage >= pageNumbers[pageNumbers.length - 1]}
+          onClick={() =>
+            currentPage < pageNumbers[pageNumbers.length - 1] &&
+            paginate(currentPage + 1)
+          }
+        />
+        <Pagination.Last
+          disabled={currentPage === pageNumbers[pageNumbers.length - 1]}
+          onClick={() =>
+            currentPage !== pageNumbers[pageNumbers.length - 1] &&
+            paginate(Math.ceil(totalAtendimentos / atendimentosPerPage))
+          }
+        />
+      </Pagination>
+    </Container>
   );
 };
 
