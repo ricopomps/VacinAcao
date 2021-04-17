59793aa1b33c4dc651b7235c61a38bc7ec55c047
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-17 14:09:12 -0300

    Implemented more tests

--- Server/src/utils/publicUtils.spec.js
+++ Server/src/utils/publicUtils.spec.js
@@ -1,8 +1,28 @@
 import { getWeekSchedules, check, getCurrentWeek } from "./publicUtils";
+import moment from "moment";
 
 describe("Teste utils", () => {
-  it("Deve retornar 7 dias", async () => {
-    const response = await getCurrentWeek(0);
-    expect(response.length).toBe(7);
+  it("Deve retornar 7 dias", () => {
+    for (let i = 0; i < 10; i++) {
+      const response = getCurrentWeek(i);
+      expect(response.length).toBe(7);
+    }
+  });
+  it("Primeiro dia ser domingo e ultimo dia ser sabado e ter o dia da semana atual", () => {
+    const week = getCurrentWeek(0);
+
+    expect(week[0].show).toBe(
+      `Domingo - ${moment().startOf("week").format("DD/MM")}`
+    );
+    expect(week[6].show).toBe(
+      `Sábado - ${moment().endOf("week").format("DD/MM")}`
+    );
+  });
+  it("Dia passado retorna danger", () => {
+    const response = check("08:00 - 08:30", {
+      day: moment().subtract(1, "days").format("DD/MM/yyyy"),
+      schedules: [],
+    });
+    expect(response).toBe("danger");
   });
 });
