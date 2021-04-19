46620338ff8e3ad386df30cc0effe0b7d9b49a85
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-19 20:55:58 -0300

    Implemented day tests

---
+++ Server/src/service/diaService/diaService.spec.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import DiaService from "./DiaService";
+import DiaModel from "../../models/dia.js";
+import dotenv from "dotenv";
+import moment from "moment";
+dotenv.config();
+
+beforeAll(async () => {
+  const CONNECTION_URL_TEST = process.env.CONNECTION_URL_TEST;
+  await mongoose.connect(CONNECTION_URL_TEST, { useNewUrlParser: true });
+});
+
+describe("Teste do service do dia", () => {
+  it("Semana de ter 7 dias", async () => {
+    for (let i = 0; i < 7; i++) {
+      const response = await DiaService.getWeek(i);
+      expect(response.body.length).toBe(7);
+      console.log(response.body);
+    }
+  });
+});
