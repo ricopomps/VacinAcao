8080f9ed9f48df824b4b2039bac926c6cfdbf5ab
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-19 22:20:26 -0300

    Added coverage to tests

--- Server/jest.config.mjs
+++ Server/jest.config.mjs
@@ -7,7 +7,7 @@ export default {
   //   module.exports = {
   //   roots: ['<rootDir>/src'],
   //   collectCoverageFrom: [ '<rootDir>/src//*.ts', '!<rootDir>/src/main/', '!<rootDir>/src//*-protocols.ts', '!/protocols/', '!<rootDir>/src/infra/db//migrations/', '!<rootDir>/src/infra/db//entities/', '!<rootDir>/src/@types/' ],
-  //   coverageDirectory: 'coverage',
+  coverageDirectory: "coverage",
   testEnvironment: "node",
   //   transform: {
   //     '.+\.ts$': 'ts-jest',
