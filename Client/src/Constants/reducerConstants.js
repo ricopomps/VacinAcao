5fb0371ff72b4489984e44549b4a251f93e063a8
ricopomps <ricardopompiliodossantos@gmail.com>
2021-04-23 16:31:09 -0300

    Created recuder constants

---
+++ Client/src/Constants/reducerConstants.js
@@ -0,0 +1,21 @@
+export const SET_AGENDAMENTOS = "SET_AGENDAMENTOS";
+
+export const SET_WEEK = "SET_WEEK";
+
+export const CHANGE_WEEK = "CHANGE_WEEK";
+
+export const SET_SEARCH = "SET_SEARCH";
+
+export const CLEAR_SEARCH = "CLEAR_SEARCH";
+
+export const SET_PAGINATION = "SET_PAGINATION";
+
+export const SET_LIMIT = "SET_LIMIT";
+
+export const SET_FORM = "SET_FORM";
+
+export const CREATE = "CREATE";
+
+export const DELETE = "DELETE";
+
+export const FINALIZAR = "FINALIZAR";
